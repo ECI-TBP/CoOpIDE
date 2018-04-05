23a5d96ea308f7bdfc1f7058cf087588b0db7b46
Juan Camilo Mantilla <juan.mantilla-r@mail.escuelaing.edu.co>
2018-04-05 12:58:59 -0500

    Avance Broker

--- src/main/resources/static/js/default.js
+++ src/main/resources/static/js/default.js
@@ -5,3 +5,22 @@ editor.session.setMode("ace/mode/java");
 function textCapture() {
     console.log(editor.getValue());
 }
+
+function init(){
+    
+}
+
+var connectAndSubscribe = function () {
+        console.info('Connecting to WS...');
+        var socket = new SockJS('/stompendpoint');
+        stompClient = Stomp.over(socket);
+        
+        stompClient.connect({}, function (frame) {
+            console.log('Connected: ' + frame);
+            stompClient.subscribe('/topic/file', function (eventbody) {
+                
+                
+            });
+        });
+
+};
